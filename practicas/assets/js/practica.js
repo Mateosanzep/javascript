f4ee3935a16849db348bef7f6963e0471ed994bb
Mateosanzep <mateosanzep@gmail.com>
2022-05-25 23:49:22 -0500

    commit

---
+++ practicas/assets/js/practica.js
@@ -0,0 +1,13 @@
+alert("hola");
+let mi_edad = 16;
+
+console.log({ mi_edad });
+
+mi_edad = 20;
+let a = 10;
+let b = 11;
+let c = 12;
+
+console.log({ mi_edad });
+console.table({ a, b, c });
+console.log(a + b);
