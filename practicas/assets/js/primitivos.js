acabfe40d284b5358274b9721dfd6f802dd382f9
Mateosanzep <97420574+Mateosanzep@users.noreply.github.com>
2022-06-15 20:38:03 -0500

    commit

---
+++ practicas/assets/js/primitivos.js
@@ -0,0 +1,8 @@
+let nombre = 'mateo sanchez';
+nombre = 'no tiene nombre';
+console.log(typeof nombre);
+let esmarvel = false;
+let edad = 16;
+let soyNull = null;
+let symbol1 = Symbol();
+let symbol2 = Symbol();
