d55d50040912e5c3c2bd239bc9d855162fe5c93e
Mateosanzep <mateosanzep@gmail.com>
2022-06-16 22:04:53 -0500

    commit

--- practicas/assets/js/funciones.js
+++ practicas/assets/js/funciones.js
@@ -1,24 +1,24 @@
-function saludar(){
-    console.log('hola');
-};
+function saludar() {
+  console.log("hola");
+}
 saludar();
 
-const saludar2 = function(){
-    confirm.log('hola');
+const saludar2 = function () {
+  confirm.log("hola");
 };
 
-function saludar3( nombre ){
-    console.log('hola ' + nombre);
-};
-saludar('mateo')
+function saludar3(nombre) {
+  console.log("hola " + nombre);
+}
+saludar("mateo");
 
-function saludar4(nombre2){
-    console.log(arguments);
-    console.log('hola' + nombre2);
-};
-nombre2('mateo', 40, true);
+function saludar4(nombre2) {
+  console.log(arguments);
+  console.log("hola" + nombre2);
+}
+saludar4("mateo", 40, true);
 
 const saludarFlecha = () => {
-    console.log('hola flecha');
+  console.log("hola flecha");
 };
 saludarFlecha();
