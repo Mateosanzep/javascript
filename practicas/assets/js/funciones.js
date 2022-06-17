810767a83e2ef57a3d3e4408d8cd7cd4013cb361
Mateosanzep <97420574+Mateosanzep@users.noreply.github.com>
2022-06-16 21:39:40 -0500

    commit

---
+++ practicas/assets/js/funciones.js
@@ -0,0 +1,24 @@
+function saludar(){
+    console.log('hola');
+};
+saludar();
+
+const saludar2 = function(){
+    confirm.log('hola');
+};
+
+function saludar3( nombre ){
+    console.log('hola ' + nombre);
+};
+saludar('mateo')
+
+function saludar4(nombre2){
+    console.log(arguments);
+    console.log('hola' + nombre2);
+};
+nombre2('mateo', 40, true);
+
+const saludarFlecha = () => {
+    console.log('hola flecha');
+};
+saludarFlecha();
